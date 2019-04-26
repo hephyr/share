5754ada186e9bf5bd52ab053033c26026c28f6b4
Zephyr <ixuhefeng@gmail.com>
2019-04-26 15:43:29 +0800

    custom port

--- share.go
+++ share.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"flag"
+	"fmt"
+	"net"
 	"net/http"
 	"os"
 	"path/filepath"
@@ -11,9 +13,11 @@ import (
 )
 
 var dir string
+var port int
 
 func init() {
 	flag.StringVar(&dir, "d", "", "set share directory")
+	flag.IntVar(&port, "p", 8080, "set server port")
 	flag.Parse()
 }
 
@@ -30,5 +34,26 @@ func main() {
 	http.Handle("/", http.RedirectHandler("/files/", http.StatusMovedPermanently))
 	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(rice.MustFindBox("static").HTTPBox())))
 	http.Handle("/files/", http.StripPrefix("/files/", serve.FileServer(serve.Dir(dir))))
-	http.ListenAndServe(":80", nil)
+
+	ipaddr := getIP()
+	fmt.Printf("Server address: http://%s:%d\n", ipaddr, port)
+
+	addr := fmt.Sprintf(":%d", port)
+	http.ListenAndServe(addr, nil)
+}
+
+func getIP() string {
+	addrs, err := net.InterfaceAddrs()
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	for _, address := range addrs {
+		if ipnet, ok := address.(*net.IPNet); ok && !ipnet.IP.IsLoopback() {
+			if ipnet.IP.To4() != nil {
+				return ipnet.IP.String()
+			}
+		}
+	}
+	return "127.0.0.1"
 }
